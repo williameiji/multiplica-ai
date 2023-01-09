6cb3e04085ef9f1f3a0a5ea0c951c4905c000945
William <william.fujimori@gmail.com>
2023-01-09 19:40:51 -0300

    feat: multiplication exercise

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        int numero = 5;
+        System.out.println("Tabela de multiplicação de " + numero);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero * i);
+        }
+    }
+}
